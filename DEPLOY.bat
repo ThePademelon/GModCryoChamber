cce523d0ce1a899a571cb13bcfdd660458f8f57f
Laura <zomanom@gmail.com>
2017-02-04 14:03:01 +1100

    Added deployment script

---
+++ DEPLOY.bat
@@ -0,0 +1,7 @@
+SET GMALOCATION=%TEMP%\chamber.gma
+E:\Steam\steamapps\common\GarrysMod\bin\gmad.exe create -folder "E:\Steam\steamapps\common\GarrysMod\garrysmod\addons\GModCryoChamber" -out %GMALOCATION% -warninvalid
+SET /P CHANGES=Release message: || GOTO CLEANUP
+E:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe update -id 850437362 -addon %GMALOCATION% -changes %CHANGES%
+
+:CLEANUP
+DEL %GMALOCATION%
